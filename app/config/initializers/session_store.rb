8926aee1e067488925c2e080d11df22d73eb8378
Federico Feroldi <federico@cloudify.me>
2011-02-05 11:58:51 +0100

    rails3 is better :)

--- app/config/initializers/session_store.rb
+++ app/config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_app_session',
-  :secret      => '07aa99f858c99e5d3ccefb99dbfd32449fb757066a19e2c69b3a83dbb350e5a832d9308b02e527262e9b13465c346788a4e4a8d426d7913445360e345ddce12c'
-}
+App::Application.config.session_store :cookie_store, :key => '_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# App::Application.config.session_store :active_record_store
