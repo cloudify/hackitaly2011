38839ca1d696cb8043de0e8049f3b0a5cbf9b955
Federico Feroldi <federico@cloudify.me>
2011-02-05 12:38:19 +0100

    new commit

--- app/app/controllers/home_controller.rb
+++ app/app/controllers/home_controller.rb
@@ -5,7 +5,15 @@ class HomeController < ApplicationController
   end
   
   def login
-    
+    breq = Typhoeus::Request.get("http://api.beintoo.com/api/rest/player/login",
+      :method        => :get,
+      :headers       => {
+        :apikey => "8efa78d626e15a7c5c72fa442f5793",
+        :guid => params[:login]
+      })
+      @current_user = JSON.parse(bresp.body)
+      
+      render 'index'
   end
   
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -49,6 +49,7 @@ App::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "home#index"
+  get "/home/login" => "home#login"
 
   # See how all your routes lay out with "rake routes"
 
