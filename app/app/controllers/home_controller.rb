0d32189c2dccc7fff9c0ed9129a03c0b9ff9c883
Federico Feroldi <federico@cloudify.me>
2011-02-05 13:16:42 +0100

    draft for chart

--- app/app/controllers/home_controller.rb
+++ app/app/controllers/home_controller.rb
@@ -1,14 +1,22 @@
 class HomeController < ApplicationController
   
+  @@apikey = "8efa78d626e15a7c5c72fa442f5793"
+  
   def index
     @current_user = session[:user] 
+    chart_req = Typhoeus::Request.get("http://api.beintoo.com/api/rest/app/topscore",
+      :method        => :get,
+      :headers       => {
+        :apikey => @@apikey
+      })
+    @top_users = ActiveSupport::JSON.decode(chart_req.body)
   end
   
   def login
     breq = Typhoeus::Request.get("http://api.beintoo.com/api/rest/player/login",
       :method        => :get,
       :headers       => {
-        :apikey => "8efa78d626e15a7c5c72fa442f5793",
+        :apikey => @@apikey,
         :guid => params[:login]
       })
       session[:user] = ActiveSupport::JSON.decode(breq.body)
